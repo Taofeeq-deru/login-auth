f37fdb7176cd8e569de336a2a05b5ea326f092b2
Taofeeq Oluderu <oluderutaofeeq@gmail.com>
2020-10-10 22:48:52 +0100

    set up login validation

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,8 +1,29 @@
 import React, { Component } from "react";
+import { validateFields } from "validator";
+
+const initialState = {
+  email: {
+    value: "",
+    validateOnChange: false,
+    error: "",
+  },
+  password: {
+    value: "",
+    validateOnChange: false,
+    error: "",
+  },
+  submitCalled: false,
+  allFieldsValidated: false,
+};
 
 class Login extends Component {
-  state = {};
+  constructor(props) {
+    super(props);
+    this.state = initialState;
+  }
+
   render() {
+    const { email, password, allFieldsValidated } = this.state;
     return (
       <>
         <div
@@ -12,7 +33,13 @@ class Login extends Component {
             className="form border border-white rounded p-3 bg-white"
             style={{ width: "320px" }}>
             <h1 className="mb-3">Log In</h1>
-            <form action="" className="w-100">
+            <div>
+              {allFieldsValidated && (
+                <p className="text-success text-center">All fields validated</p>
+              )}
+            </div>
+            {/**form starts here */}
+            <form onSubmit={(e) => this.handleSubmit(e)} className="w-100">
               <div className="form-group">
                 <label htmlFor="email" className="sr-only">
                   Email
@@ -21,9 +48,23 @@ class Login extends Component {
                   type="email"
                   name="email"
                   id="email"
-                  className="form-control"
+                  value={email.value}
+                  className={`form-control ${
+                    email.error === false
+                      ? "is-valid"
+                      : email.error
+                      ? "is-invalid"
+                      : ""
+                  }`}
                   placeholder="Enter Email"
+                  onChange={(e) =>
+                    this.handleChange(validateFields.validateEmail, e)
+                  }
+                  onBlur={(e) =>
+                    this.handleBlur(validateFields.validateEmail, e)
+                  }
                 />
+                <div className="invalid-feedback">{email.error}</div>
               </div>
               <div className="form-group">
                 <label htmlFor="password" className="sr-only">
@@ -33,12 +74,29 @@ class Login extends Component {
                   type="password"
                   name="password"
                   id="password"
-                  className="form-control"
-                  placeholder="Enter Password"
+                  value={password.value}
+                  className={`form-control ${
+                    password.error === false
+                      ? "is-valid"
+                      : password.error
+                      ? "is-invalid"
+                      : ""
+                  }`}
+                  placeholder="Enter Password "
+                  onChange={(e) =>
+                    this.handleChange(validateFields.validatePassword, e)
+                  }
+                  onBlur={(e) =>
+                    this.handleBlur(validateFields.validatePassword, e)
+                  }
                 />
+                <div className="invalid-feedback">{password.error}</div>
               </div>
               <div className="form-group">
-                <button type="submit" className="btn btn-primary form-control">
+                <button
+                  type="submit"
+                  className="btn btn-primary form-control"
+                  onMouseDown={() => this.setState({ submitCalled: true })}>
                   Log In
                 </button>
               </div>
